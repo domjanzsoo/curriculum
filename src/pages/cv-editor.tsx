0c38c31f4a979bc84628c7b01427d85f04313781
Zsolt Domjan <zsolt.domjan@elogs.co.uk>
2023-12-10 00:27:01 +0000

    details and cv-editor page creation

---
+++ src/pages/cv-editor.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const CVEditorPage = () => {
+    return (
+        <main>
+            <h2>Editor page...</h2>
+        </main>
+    )
+}
+
+export default CVEditorPage
